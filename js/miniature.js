b113bc1b1c4170ddb96966a5eeb481454d374a9a
Eldar Dusmuratov <eldardusmuratov@gmail.com>
2024-01-17 15:39:17 +0300

    add alt text

--- js/miniature.js
+++ js/miniature.js
@@ -2,9 +2,10 @@ const miniatureTemplate = document
   .querySelector('#picture')
   .content.querySelector('.picture');
 const box = document.querySelector('.pictures');
-const createMiniature = ({url, likes, comments}) => {
+const createMiniature = ({url, likes, comments, description}) => {
   const miniature = miniatureTemplate.cloneNode(true);
   miniature.querySelector('.picture__img').src = url;
+  miniature.querySelector('.picture__img').alt = description;
   miniature.querySelector('.picture__likes').textContent = likes;
   miniature.querySelector('.picture__comments').textContent = comments.length;
   return miniature;
