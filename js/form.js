960a2a27a3f0da64618caf453b865a9ed9eb39fc
Eldar Dusmuratov <eldardusmuratov@gmail.com>
2024-03-10 18:06:01 +0300

    Upload file

---
+++ js/form.js
@@ -0,0 +1,102 @@
+// import Pristine from '../vendor/pristine/pristine.min.js';
+const MAX_HASHTAG_COUNT = 5;
+const VALID_SYMBOLS = /^#[a-za-яë0-9]{1,19}$/i;
+const errorText = {
+  INVALID_COUNT: `Максимум ${MAX_HASHTAG_COUNT} хэштегов`,
+  NOT_UNIQUE: 'Хэштеги должны быть уникальными',
+  INVALID_PATTERN: 'Неправильный хэштег',
+};
+
+const body = document.querySelector('body');
+const form = document.querySelector('.img-upload__form');
+const overlay = form.querySelector('.img-upload__overlay');
+const cancelButton = form.querySelector('.img-upload__cancel');
+const fileField = form.querySelector('.img-upload__input');
+const hashtagField = form.querySelector('.text__hashtags');
+const commentField = form.querySelector('.text_description');
+
+const pristine = new Pristine(form, {
+  classTo: 'img-upload__field-wrapper',
+  errorTextParent: 'img-upload_field-wrapper',
+  errorTextClass: 'img-upload__field-wrapper_error',
+});
+
+
+const showModal = () => {
+  overlay.classList.remove('hidden');
+  body.classList.add('modal-open');
+  document.addEventListener('keydown', onDocumentKeydown);
+};
+
+const hideModal = () => {
+  form.reset();
+  pristine.reset();
+  overlay.classList.add('hidden');
+  body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onDocumentKeydown);
+};
+
+const isTextFieldFocused = () =>
+  document.activeElement === hashtagField ||
+  document.activeElement === commentField;
+
+function onDocumentKeydown(evt) {
+  if (evt.key === 'Escape' && !isTextFieldFocused()) {
+    evt.preventDefault();
+    hideModal();
+  }
+}
+
+const normalizeTags = (tagString) => tagString
+  .trim()
+  .split(' ')
+  .filter((tag) => Boolean(tag.length));
+
+const hasValidTags = (value) => normalizeTags(value).every((tag) => VALID_SYMBOLS.test(tag));
+
+const hasValidCount = (value) => normalizeTags(value).length <= MAX_HASHTAG_COUNT;
+
+const hasUniqueTags = (value) => {
+  const lowerCaseTags = normalizeTags(value).map((tag) => tag.toLowerCase());
+  return lowerCaseTags.length === new Set(lowerCaseTags).size;
+};
+
+const onCancelButtonClick = () => {
+  hideModal();
+};
+
+const onFileInputChange = () => {
+  console.log('slkjs');
+  showModal();
+};
+
+const onFormSubmit = (evt) => {
+  evt.preventDefault();
+  pristine.validate();
+};
+
+pristine.addValidator(
+  hashtagField,
+  hasValidCount,
+  errorText.INVALID_COUNT,
+  3,
+  true
+);
+pristine.addValidator(
+  hashtagField,
+  hasUniqueTags,
+  errorText.NOT_UNIQUE,
+  2,
+  true
+);
+pristine.addValidator(
+  hashtagField,
+  hasValidTags,
+  errorText.INVALID_PATTERN,
+  1,
+  true
+);
+
+fileField.addEventListener('change', onFileInputChange);
+cancelButton.addEventListener('click', onCancelButtonClick);
+form.addEventListener('submit', onFormSubmit);
