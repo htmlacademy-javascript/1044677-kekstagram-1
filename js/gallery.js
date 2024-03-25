6ac22bdf9b3a7eeb1e43092df313eae2cf4a89da
Eldar Dusmuratov <eldardusmuratov@gmail.com>
2024-03-25 19:13:06 +0300

    Add Sort

--- js/gallery.js
+++ js/gallery.js
@@ -3,18 +3,25 @@ import { showBigPhoto } from './big_photo.js';
 
 const box = document.querySelector('.pictures');
 
-const renderGallery = (photos) => {
-  box.addEventListener('click', (evt) => {
-    const miniature = evt.target.closest('[data-miniature-id]');
-    if (!miniature) {
-      return;
-    }
-    const photo = photos.find(
-      (item) => item.id === +miniature.dataset.miniatureId);
-    showBigPhoto(photo);
-  });
+let photos = [];
 
+const onBoxClick = (evt) => {
+  const miniature = evt.target.closest('[data-miniature-id]');
+  if (!miniature) {
+    return;
+  }
+
+  evt.preventDefault();
+  const photo = photos.find(
+    (item) => item.id === +miniature.dataset.miniatureId
+  );
+  showBigPhoto(photo);
+};
+
+const renderGallery = (currentPhotos) => {
+  photos = currentPhotos;
   renderMiniatures(photos, box);
+  box.addEventListener('click', onBoxClick);
 };
 
 export {renderGallery};
